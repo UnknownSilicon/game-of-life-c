41fa94bf9f79f6f74b39d14fcf430c0f7f5c5154
jakebacker <jbacker42@gmail.com>
2020-06-13 01:13:18 -0400

    Add new initialization code

--- game.c
+++ game.c
@@ -12,16 +12,28 @@ void initialize() {
     FILE* f = fopen("C:/Users/Jake/Documents/Programming/gameOfLifeFAST/input.txt", "r");
 
     fscanf(f, "%d", &size);
+    size = (size+1)/2; // Divide by 2 and round up
 
     board = malloc(size * sizeof(char*));
 
     for(int i=0; i<size; i++) {
         board[i] = malloc(size * sizeof(char));
 
-        int val;
-        for (int j=0; j<size; j++) {
+        // This can actually be made even faster since the only possible values are 1 or 0
+        unsigned int temp = 0;
+        unsigned int val;
+        unsigned int combine_mask = 0;
+        int j=0;
+        while(j<size) {
             fscanf(f, "%d", &val);
-            board[i][j] = (char)val;
+            temp = ((temp & combine_mask) | val) | (val & ~combine_mask); // Store either temp, or val in temp based on the mask
+
+            temp = temp << (4u & ~combine_mask);
+
+            board[i][j] = (char)temp;
+
+            j += combine_mask != 0;
+            combine_mask = ~combine_mask;
         }
     }
 
@@ -43,23 +55,24 @@ void run(int iterations) {
             // Set all values
             for (unsigned int y = 0; y < size; y++) {
                 for (unsigned int x = 0; x < size; x++) {
+                    // TODO: Rewrite this bit to take into account the shifted values
                     if (board[y][x] % 2) { // Is cell alive?
                         // Increment all adjacent cells by 2. Also check to make sure they exist
                         unsigned int dy = y - 1;
                         unsigned int dx = x - 1;
-                        if (dx < size && dy < size) board[dy][dx] += 2;
-                        if (++dx < size && dy < size) board[dy][dx] += 2;
-                        if (++dx < size && dy < size) board[dy][dx] += 2;
+                        if (dx < size && dy < size && board[dy][dx] < 8) board[dy][dx] += 2;
+                        if (++dx < size && dy < size && board[dy][dx] < 8) board[dy][dx] += 2;
+                        if (++dx < size && dy < size && board[dy][dx] < 8) board[dy][dx] += 2;
                         dx = x - 1;
                         dy++;
-                        if (dx < size && dy < size) board[dy][dx] += 2;
+                        if (dx < size && dy < size && board[dy][dx] < 8) board[dy][dx] += 2;
                         dx++; // Skip the middle
-                        if (++dx < size && dy < size) board[dy][dx] += 2;
+                        if (++dx < size && dy < size && board[dy][dx] < 8) board[dy][dx] += 2;
                         dx = x - 1;
                         dy++;
-                        if (dx < size && dy < size) board[dy][dx] += 2;
-                        if (++dx < size && dy < size) board[dy][dx] += 2;
-                        if (++dx < size && dy < size) board[dy][dx] += 2;
+                        if (dx < size && dy < size && board[dy][dx] < 8) board[dy][dx] += 2;
+                        if (++dx < size && dy < size && board[dy][dx] < 8) board[dy][dx] += 2;
+                        if (++dx < size && dy < size && board[dy][dx] < 8) board[dy][dx] += 2;
                     }
                 }
             }
